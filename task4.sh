6bcad0eb5bf76a3d3118acf091ea83a9cbdadffe
Mboowa Haulah N <106828953+MboowaHaulah@users.noreply.github.com>
2024-08-26 19:40:48 +0300

    Add files via upload

---
+++ task4.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+####################################
+# Name: <MBOOWA HAULAH NAMAGGA>
+# BSE 4105 - 2024 
+# Exercise 1 - Task 4
+####################################
+
+function problem1 {
+  # Type your answer to problem #1 below this line
+  cp MyProgram.java java/
+  echo "Not yet implemented"
+}
+
+function problem2 {
+  # Type your answer to problem #2 below this line
+  ls -l
+  echo "Not yet implemented"
+}
+
+function problem3 {
+  # Type your answer to problem #3 below this line
+  ls -lar /var
+  echo "Not yet implemented"
+}
+
+function problem4 {
+  # Type your answer to problem #4 below this line
+  mv Burrot.java Borat.java
+  echo "Not yet implemented"
+}
+
+function problem5 {
+  # Type your answer to problem #5 below this line
+  rm diff.html diff.css
+  echo "Not yet implemented"
+}
+
+function problem6 {
+  # Type your answer to problem #6 below this line
+  touch -t 202001010415 MyProgram.java
+  echo "Not yet implemented"
+}
+
+function problem7 {
+  # Type your answer to problem #7 below this line
+  ls *.html *.css
+  echo "Not yet implemented"
+}
+
+function problem8 {
+  # Type your answer to problem #8 below this line
+  cp *.txt java/
+  echo "Not yet implemented"
+}
+
+function problem9 {
+  # Type your answer to problem #9 below this line
+  diff lyrics.txt lyrics2.txt
+  echo "Not yet implemented"
+}
+
+function problem10 {
+  # Type your answer to problem #10 below this line
+  cat lyrics.txt
+  echo "Not yet implemented"
+}
+
+function problem11 {
+  # Type your answer to problem #11 below this line
+  cat song*.txt
+  echo "Not yet implemented"
+}
+
+function problem12 {
+  # Type your answer to problem #12 below this line
+  head -n 7 animals.txt
+  echo "Not yet implemented"
+}
